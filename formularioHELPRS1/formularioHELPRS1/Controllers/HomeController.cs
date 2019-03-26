a0c05dcdd7f131f1b50689556c11a03503ddde9a
micke alvin nuñez reyes <alvin10_26@hotmail.com>
2019-03-26 11:17:24 -0400

    tarea casi terminada

---
+++ formularioHELPRS1/formularioHELPRS1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using formularioHELPRS1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace formularioHELPRS1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Resultado(persona obj, FormCollection collection)
+        {
+            string hobbys = collection["Hobbys"];
+
+            obj.Hobbys = hobbys;
+            return View(obj);
+        }
+
+        public ActionResult persona()
+        {
+            List<string> pasatiempos = new List<string>();
+
+
+            pasatiempos.Add("Programar");
+            pasatiempos.Add("Estudiar");
+            pasatiempos.Add("Escuchar Musica");
+            pasatiempos.Add("Comer");
+
+            ViewBag.pasatiempo = pasatiempos;
+            return View();
+        }
+    }
+}
---
+++ formularioHELPRS1/formularioHELPRS1/Models/persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace formularioHELPRS1.Models
+{
+    public class persona
+    {
+        [Required]
+        public int Cedula { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Range(16, 99)]
+        public int Edad { get; set; }
+        public string Telefono { get; set; }
+        [EmailAddress]
+        public string Correo { get; set; }
+        public string Estado_civil { get; set; }
+        public string Hobbys { get; set; }
+        //genero
+        public Gender genero { get; set; }
+
+    }
+    public enum Gender
+    {
+        Masculino,
+        Femenino
+    }
+
+}
